8944dbc61fc1ecc095e3479002e293c1d56abba8
Rob Weber <robweberjr@gmail.com>
2021-07-02 19:48:04 +0000

    community is required snmp string check switch uptime

--- modules/devices/switch_device.py
+++ modules/devices/switch_device.py
@@ -1,3 +1,5 @@
+import os
+import modules.utils as utils
 from .. device import DRDevice
 
 
@@ -5,14 +7,27 @@ class SwitchDevice(DRDevice):
     """
     Represents a Cisco style network switch
     """
+    community = None
+
     def __init__(self, name, address, config):
         super().__init__(name, address, "switch")
+
+        self.community = config['community']
 
         self.info = ("This device type will work with Cisco branded switches. SNMP information must be correct"
                      "and setup on the switch for services to properly be queried.")
 
     def _custom_checks(self):
-        return []
+        result = []
+
+        # basic args all scripts need
+        args = ["-H", self.address, "-c", self.community]
+
+        # check system uptime
+        output = self._run_process(os.path.join(utils.DIR_PATH, "check_scripts", "check_snmp.py"), args + ['-o', '1.3.6.1.2.1.1.3.0'])
+        result.append({"return_code": output.returncode, "text": output.stdout, "name": "Switch Uptime"})
+
+        return result
 
     def _get_services(self):
         return ["Uptime"]
