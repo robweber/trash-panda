6a92fcf64ca6405453b0c092d4bbd8be2a9c912a
Rob Weber <robweberjr@gmail.com>
2024-11-18 09:31:51 -0600

    highlight countdown if it's passed expected update time

--- web/static/js/countdown.js
+++ web/static/js/countdown.js
@@ -1,6 +1,6 @@
 /*
 use this to display a countdown timer
-page must contain an element "last_updated" to display the countdown in seconds
+constructor expects the name of a container to update with the countdown info
 implementing class should call "reset_countdown()" to reset the timer to 0
 
 start countdown with call to start_countdown()
@@ -8,9 +8,11 @@ start countdown with call to start_countdown()
 
 class UpdateCountdown {
 
-  constructor(target_div){
+  constructor(target_div, expected_update = 15, stale_class = 'text-danger'){
     this.UPDATE_COUNTDOWN = 0;
     this.TARGET_DIV = target_div;  // div to update countdown info
+    this.RESET_TIMEOUT = expected_update;  // expected amount of time before reset
+    this.STALE_CLASSNAME = stale_class;
   }
 
   start_countdown(){
@@ -20,10 +22,17 @@ class UpdateCountdown {
   show_countdown(){
     this.UPDATE_COUNTDOWN ++;
     $(`#${this.TARGET_DIV}`).html(this.UPDATE_COUNTDOWN);
+
+    if(this.UPDATE_COUNTDOWN > this.RESET_TIMEOUT)
+    {
+      $(`#${this.TARGET_DIV}`).addClass(this.STALE_CLASSNAME);
+    }
   }
 
   reset_countdown(){
+    // reset countdown and remove stale class (if exists)
     this.UPDATE_COUNTDOWN = 0;
+    $(`#${this.TARGET_DIV}`).removeClass(this.STALE_CLASSNAME);
   }
 
 }
